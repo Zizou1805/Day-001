02257f0d612098c8be233eb8155f3cb41fb89682
Zizou1805 <64688039+Zizou1805@users.noreply.github.com>
2020-05-04 16:38:52 +0530

    Initial commit

---
+++ src/main/java/CodeNoteDemo.java
@@ -0,0 +1,12 @@
+/*
+ * Demo Class
+ *
+ * @author : Shekhar
+ * @date : 2020/05/05
+ */
+
+public class CodeNoteDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello, Shekhar!!!");
+    }
+}
